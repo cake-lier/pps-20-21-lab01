5852df2c787d71d1bb2d47354e96cf933cae4cce
Matteo Castellucci <matteo.castellucci3@studio.unibo.it>
2021-03-01 13:07:47 +0100

    Add implementation of CircularList

--- pps-lab01-tdd/test/CircularListTest.java
+++ pps-lab01-tdd/test/CircularListTest.java
@@ -1,4 +1,7 @@
+import lab01.tdd.CircularList;
+import lab01.tdd.CircularListImpl;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
@@ -6,12 +9,10 @@ import org.junit.jupiter.api.Test;
  * The test suite for testing the CircularList implementation
  */
 public class CircularListTest {
+    public CircularList list;
 
-    //TODO: test implementation
-
-    @Disabled
-    @Test public void testTodo(){
-        Assertions.fail();
+    @BeforeEach
+    void setUp() {
+        list = new CircularListImpl();
     }
-
 }
